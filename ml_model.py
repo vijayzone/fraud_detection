d9f666402bf691e4d0f7b9a0c7435f20beff72e7
vijayzone <vijay_zone@yahoo.com>
2022-04-22 09:53:16 +0530

    dded 3 ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("Adding all ML related code here")
+
+def model1():
+    print("Adding code for ML model1")
+
+def model2():
+    print("Adding code for ML model1")
+
+def model3():
+    print("Adding code for ML model1")
+            
